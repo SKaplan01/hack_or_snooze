5fc1674bad33333cf9ad4647e09c681573d24756
Sarah Kaplan <skaplan520@gmail.com>
2018-09-17 16:30:50 -0700

    added domain name filtering

--- hacker.js
+++ hacker.js
@@ -1,26 +1,47 @@
 //if both title and url have a value, calls create AndAppendNewLi
-function validateInput(title, url) {
+function validateInput(title, url, domain) {
   if (title === '' || url === '') {
     alert('Please enter a valid title and url');
   } else {
-    createAndAppendNewLi(title, url);
+    createAndAppendNewLi(title, url, domain);
+    $('#container-submit').slideToggle();
+    $('form').trigger('reset');
   }
 }
 
-function createAndAppendNewLi(title, url) {
+function createAndAppendNewLi(title, url, domain) {
   let newListItem = $('<li>');
   let newFavoriteIcon = $('<i>');
-  newFavoriteIcon.addClass('far fa-star');
+  newFavoriteIcon.addClass('far fa-star text-muted');
+
+  let newLink = $('<a>');
+  newLink.attr('href', url);
 
   let newTitle = $('<span>');
   newTitle.text(title);
 
   let newSmallText = $('<small>');
-  newSmallText.text(url);
+  newSmallText.text(`(${domain})`);
+  newSmallText.addClass('text-muted');
 
-  newListItem.append(newFavoriteIcon, newTitle, newSmallText);
+  newLink.append(newTitle);
+  newListItem.append(newFavoriteIcon, newLink, newSmallText);
+  newListItem.children().addClass('pl-2');
 
   $('ol').append(newListItem);
+}
+
+function trimDomainName(url) {
+  url = url.replace('https://', '');
+  url = url.replace('http://', '');
+  url = url.replace('www.', '');
+  return url.split('/')[0];
+}
+
+function filterByDomainName(event) {
+  let domainName = $(event.target).text();
+  $(`ol li:contains(${domainName})`).addClass('same-domain');
+  $('#container-list li:not(.same-domain)').addClass('li-hidden-domain');
 }
 
 $(document).ready(function() {
@@ -33,8 +54,8 @@ $(document).ready(function() {
     event.preventDefault();
     let title = $('#title').val();
     let url = $('#url').val();
-    validateInput(title, url);
-    $('form').trigger('reset');
+    let domain = trimDomainName(url);
+    validateInput(title, url, domain);
   });
 
   $('ol').on('click', 'i', function(event) {
@@ -45,6 +66,12 @@ $(document).ready(function() {
   });
 
   $('#show-favorites').on('click', function(event) {
-    $('#container-list li:not(.favorite)').toggleClass('li-hidden');
+    $('#container-list li:not(.favorite)').toggleClass('li-hidden-favorites');
+  });
+
+  $('ol').on('click', 'small', filterByDomainName);
+
+  $('#home-button').on('click', function(event) {
+    $('li').removeClass('li-hidden-favorites li-hidden-domain same-domain');
   });
 });
